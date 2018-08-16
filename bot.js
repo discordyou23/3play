e9644a4d66a39cca44209cfa0e0f63bbd649581d
discordyou23 <42048326+discordyou23@users.noreply.github.com>
2018-08-16 17:09:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,24 +11,9 @@ client.on('ready', () => {
     
  
 
-    
- client.on('ready', function(){    
-    var ms = 40000 ;    
-    var setGame = ['Fhelp|لبيك الله هم لبيك'];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
-}, ms);    
-    
-});   
+client.on('ready', () => {
+client.user.setGame(`Fhelp`);
+});
 
 
 
