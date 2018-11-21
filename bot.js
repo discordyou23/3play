13d92b9eb146ea1a0b3c5821d23bc09900c57b51
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-11-21 21:08:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,11 @@ fofo.on('ready', () => {
   console.log('              Bot Is Online')
   console.log('            ╚[════════════]╝')
   console.log('')
-  console.log(`Logged in as ${fofo.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
   console.log('')
-  console.log(`servers! [ " ${fofo.guilds.size} " ]`);
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
   console.log('')
-  console.log(`Users! [ " ${fofo.users.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
   console.log('')
   console.log('╚[════════════════════════════════════]╝')
 });
@@ -21,44 +21,44 @@ fofo.on('ready', () => {
 
 
 
-fofo.on('ready', () => {                           
-fofo.user.setGame(`_Mr_Fawzo🍻|_I_am_Strong<_*🍺`,'https://www.twitch.tv/fofodiscord');                                                                                                                                                                                                                                                                                                                                                                                                                            
+client.on('ready', () => {                           
+client.user.setGame(`_Mr_Fawzo🍻|_I_am_Strong<_*🍺`,'https://www.twitch.tv/fofodiscord');                                                                                                                                                                                                                                                                                                                                                                                                                            
 });
 
 
 
 const developers = ["418418238267850752",""]
-fofo.on('message', message => {
+client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       //fofo©
   if (message.content.startsWith(adminprefix + 'sp')) {
-    fofo.user.setGame(argresult);
+    client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else 
      if (message.content === (adminprefix + "leave")) {//fofo©
     message.guild.leave();        
   } else
   if (message.content.startsWith(adminprefix + 'st')) {
-    fofo.user.setGame(argresult, "https://www.twitch.tv/fofodiscord");//fofo©
+    client.user.setGame(argresult, "https://www.twitch.tv/fofodiscord");//fofo©
       message.channel.send(`**✅**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
-  fofo.user.setUsername(argresult).then
+  client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
 if (message.content.startsWith(adminprefix + 'setavatar')) {
-  fofo.user.setAvatar(argresult);
+  client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);//fofo©
 }
 });
 //fofo©
 var KinG66S = {};
-fofo.on('guildMemberRemove', member => {
+client.on('guildMemberRemove', member => {
 KinG66S[member.id] = {roles: member.roles.array()};
 });
 
-fofo.on('guildMemberAdd', member => {//fofo©
+client.on('guildMemberAdd', member => {//fofo©
 if(!KinG66S[member.user.id]) return;
 console.log(KinG66S[member.user.id].roles.length);
 for(let i = 0; i < KinG66S[member.user.id].roles.length + 1; i++) {
@@ -111,21 +111,21 @@ SPAMMMMMMM
 
 */
 
-fofo.on('warn', console.warn);
+client.on('warn', console.warn);
 
-fofo.on('error', console.error);//by fofodiscord
+client.on('error', console.error);//by fofodiscord
 
-fofo.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By BlackSpirit','\n','ProBot credits miner is ready!'));
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By BlackSpirit','\n','ProBot credits miner is ready!'));
 
-fofo.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
 
-fofo.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
 
 function timerFunc() {//by fofodiscord
 
-    fofo.on('message', msg => {
+    client.on('message', msg => {
 
-        fofo.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
 
     });
 
@@ -141,4 +141,4 @@ var timer = setTimeout(timerFunc, 1000);
 
 
 
-fofo.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
