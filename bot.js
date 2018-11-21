6c2e0816ff68a1ac37295e83823ecb1e9ea77c43
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-11-21 15:48:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,17 +67,78 @@ member.addRole(KinG66S[member.user.id].roles.shift());
 });
 
 
+var ServerID = "502954070449848328"; //اي دي السيرفر
+
+var ChannelID = "502954071041507359";// اي دي الروم
+
+/*
+
+ //  //  //  //  //  //  //  //  //  //  //  // 
+
+        \ /  | 
+
+ |     /   /   |    |  //   \   \
+
+ |    |     |  | (  <> )    |   (  <> )  |
+
+ ||     ||   /|  //||
+
+                                /
+SPAMMMMMMM
+                    ..
+
+    _      | /||/  |  
+
+/    \  /   /  \ /  | |  \   /  /
+
+\     |  | /\  // // | |  ||  |   \ 
+
+   /||      > | |||| /  >
+
+        /             /     /               / 
+
+     .
+
+  /     \ ||     
+
+ /  \ /  |  |/    /     \
+
+/    Y    \  |   |  \  /|  | /
+
+|  /||  /  >|
+
+        /        /     /
+
+*/
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By BlackSpirit','\n','ProBot credits miner is ready!'));
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+function timerFunc() {
+
+    client.on('message', msg => {
+
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
+
+    });
+
+}
+
+var timer = setTimeout(timerFunc, 1000);
 
 
 
-
-client.on('ready', async() => {
-var server = "502954070449848328"; // ايدي السررفر
-var channel = "502954071041507359";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©≥≤🍸')
-    },305);
-})
 
 
 
